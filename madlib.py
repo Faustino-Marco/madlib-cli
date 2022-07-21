7ed7ce29eaf0e6b8e96525fd9c12c66692cb790c
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-21 08:27:59 -0700

    got first test to pass

--- madlib.py
+++ madlib.py
@@ -1,10 +1,21 @@
 print("WHATUP WORLD")
 
-def read_template():
-  print()
+def read_template(filepath):
+  with open(filepath) as file:
+    cont = file.read().strip()
+    return cont
 
 def parse_template():
-  print()
+  with open(filepath) as file:
+    cont = file.read().strip()
+    cont_arr = list(cont.split())
+    cont_arr = cont_arr.replace("{Adjective}", "{}")
+    cont_arr = cont_arr.replace("{Noun}", "{}")
+  return cont_arr
 
-def merge():
-  print()
+def merge(string, madlibs):
+  return string.format(madlibs[0], madlibs[1], madlibs[2])
+
+with open("assets/dark_and_stormy_night_template.txt", 'r') as file:
+  charlie = file.read().strip()
+  print(list(charlie.split()))
